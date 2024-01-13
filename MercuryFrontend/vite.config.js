f12c80d34a958dd3c4638a4168e78072ce075809
Mateusz Kołacz <58779750+Matimateokol@users.noreply.github.com>
2024-01-13 20:55:55 +0100

    Update vite.config.js
    Add removed comment

--- MercuryFrontend/vite.config.js
+++ MercuryFrontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   ],
   server: {
     host: true,
-    port: 3000,
+    port: 3000, // Port used by docker container
     strictPort: true,
     watch: {
       usePolling: true,
