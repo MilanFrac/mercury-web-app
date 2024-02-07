9012514ed08b86363276f9898c098285deed27dc
Mateusz Kołacz <58779750+Matimateokol@users.noreply.github.com>
2024-02-07 21:12:46 +0100

    Update vite.config.js

--- MercuryFrontend/vite.config.js
+++ MercuryFrontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     'process.env.REACT_APP_BACKEND_API_BASE_URL': `"${process.env.REACT_APP_BACKEND_API_BASE_URL}"`
   },
   plugins: [react()],
-  base: '/MercuryFrontend/'
+  base: '/MercuryFrontend/',
   server: {
     host: true,
     port: 3000, // Port used by docker container
