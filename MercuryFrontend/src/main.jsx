6a3b692f829a875bb8f8742adff439d7c3b8fef6
Matimateokol <58779750+Matimateokol@users.noreply.github.com>
2024-02-07 22:51:24 +0100

    Fix browser routing

--- MercuryFrontend/src/main.jsx
+++ MercuryFrontend/src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import App from './App';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = document.getElementById('root');
 const rootInstance = createRoot(root);
 
 rootInstance.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/mercury-web-app">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
